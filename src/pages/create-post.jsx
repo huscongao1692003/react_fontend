9453cee63c468aa8d4ebab9671dc3dc902f41a2c
Thanh Nguyen <89590291+thanhnguyen1609@users.noreply.github.com>
2023-10-23 17:00:26 +0700

    create-post

--- src/pages/create-post.jsx
+++ src/pages/create-post.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import SEO from "../common/seo";
 import WrapperFour from "../layout/wrapper-4";
+import PostCreate from "../components/post";
 
 const index = () => {
     return (
         <WrapperFour>
             <SEO pageTitle={"Create Post"} />
-           //add later
+            <PostCreate />
         </WrapperFour>
         );
 };
