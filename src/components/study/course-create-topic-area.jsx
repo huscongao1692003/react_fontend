e236acdb81c01d5d52914aa71f57be3a5640a8c2
Thanh Nguyen <89590291+thanhnguyen1609@users.noreply.github.com>
2023-10-26 13:51:41 +0700

    study , lesson with videoplayer, pdf reader

--- src/components/study/course-create-topic-area.jsx
+++ src/components/study/course-create-topic-area.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import VideoPlayer from "@/src/components/video-player/VideoPlayer";
+import { Document, Page } from "react-pdf";
 
 function CourseCreateTopicArea() {
   const router = useRouter();
   const { id } = router.query;
   const [courseData, setCourseData] = useState([]);
+  const [showLessons, setShowLessons] = useState(false);
 
   const getCourseData = async () => {
     const url = `https://drawproject-production.up.railway.app/api/v1/courses/${id}/topic`;
@@ -35,6 +37,10 @@ function CourseCreateTopicArea() {
     }
   }, [id]);
 
+  const toggleLessonsVisibility = () => {
+    setShowLessons(!showLessons);
+  };
+
   return (
     <div style={{ minHeight: "50vh" }}>
       {courseData.map((item, index) => (
@@ -42,42 +48,50 @@ function CourseCreateTopicArea() {
           <div className="card-header d-flex justify-content-between align-items-center">
             <p className="mb-0 font-weight-bold">{item.topicTitle}</p>
             <div>
-              <button type="button" className="btn btn-sm btn-outline-success m-1">
+              <button onClick={toggleLessonsVisibility} type="button" className="btn btn-sm btn-outline-success m-1">
                 See Lessons
               </button>
             </div>
           </div>
-          <div className="card-body">
-            {item.lessons.map((lesson, lessonIndex) => (
-              <div key={lessonIndex}>
-                <div className="d-flex align-items-center justify-content-between">
-                  <div className="d-flex align-items-center">
-                    <div className="icon"></div>
-                    <div className="ms-4">
-                      <div className="text-xs text-muted" style={{ fontWeight: "600" }}>
-                        Lesson {lessonIndex + 1}: {lesson.name}
-                      </div>
-                      <div className="text-xs text-muted">{lesson.typeFile}</div>
-                      <div>
-                        <h1>Video Player Example</h1>
-                        <VideoPlayer videoUrl={"https://www.youtube.com/watch?v=Y4k3aM0Su58"} />
+          {showLessons && (
+            <div className="card-body">
+              {item.lessons.map((lesson, lessonIndex) => (
+                <div key={lessonIndex}>
+                  <div className="d-flex align-items-center justify-content-between">
+                    <div className="d-flex align-items-center">
+                      <div className="icon"></div>
+                      <div className="ms-4">
+                        <div className="text-xs text-muted" style={{ fontWeight: "600" }}>
+                          Lesson {lessonIndex + 1}: {lesson.name}
+                        </div>
+                      
+                        <div>
+                          {lesson.typeFile === "video" ? (
+                            <div>
+                              <h1>Video Player Example</h1>
+                              <VideoPlayer videoUrl={lesson.url} />
+                            </div>
+                          ) : lesson.typeFile === "pdf" ? (
+                            <div>
+                              <h1>PDF Viewer</h1>
+                              <Document file={lesson.url}>
+                                <Page pageNumber={1} />
+                              </Document>
+                            </div>
+                          ) : null}
+                        </div>
                       </div>
                     </div>
                   </div>
-                  <div className="ms-4 small">
-                    <a href="javascript:void(0);" className="me-3">
-                      Study
-                    </a>
-                  </div>
+                  {lessonIndex + 1 !== item.lessons.length && <hr />}
                 </div>
-                {lessonIndex + 1 !== item.lessons.length && <hr />}
-              </div>
               ))}
-          </div>
+            </div>
+          )}
         </div>
-        ))}
+      ))}
     </div>
-    );
+  );
 }
 
 export default CourseCreateTopicArea;
--- src/components/video-player/VideoPlayer.jsx
+++ src/components/video-player/VideoPlayer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 
 const VideoPlayer = ({ videoUrl }) => {
-    return <ReactPlayer url={videoUrl} width="200%" height="500px" controls />;
+    return <ReactPlayer url={videoUrl} width="100%" height="500px" controls />;
 };
 
 export default VideoPlayer;
